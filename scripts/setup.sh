1936c298eb2361d789df9e76491402dc1cb3265f
“JINGFFF” <15827238926@163.com>
2021-04-09 17:12:27 +0800

    2018

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,9 @@ export PATH="${PATH}:${PWD}/aqgc"
 export PATH="${PATH}:${PWD}/fakephoton"
 export PATH="${PATH}:${PWD}/make_datacard"
 export PATH="${PATH}:${PWD}/uncertainty"
+export PATH="${PATH}:${PWD}/condor"
 chmod +x aqgc/*py
 chmod +x fakephoton/*py
 chmod +x make_datacard/*py
 chmod +x uncertainty/*py 
+chmod +x condor/*py
