cb4d28119947f792ad6da5d3683285fe13ea12dd
“JINGFFF” <15827238926@163.com>
2021-06-02 17:07:45 +0800

    delete 7

--- step7_significance/uncer5/te1/combine/im/impacts_combine.sh
+++
@@ -1,5 +0,0 @@
-text2workspace.py $1 -m 125
-combineTool.py -M Impacts --expectSignal=1 -t -1 -d datacard_combine.root -m 125 --doInitialFit
-combineTool.py -M Impacts --expectSignal=1 -t -1 -d datacard_combine.root -m 125 --doFits --parallel 4
-combineTool.py -M Impacts --expectSignal=1 -t -1 -d datacard_combine.root -m 125 -o impacts.json
-plotImpacts.py -i impacts.json -o impacts
